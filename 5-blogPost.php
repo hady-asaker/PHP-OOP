99b1dbfbe5c13813d23d7bc23a63467ded20fdce
hady-asaker <hadyasaker8@gmail.com>
2023-04-26 01:19:51 +0200

    ADD blog bost problem

---
+++ 5-blogPost.php
@@ -0,0 +1,8 @@
+<?php
+/* ------------------------------------------------------------------------------------
+  Problem 5: Create a class that represents a blog post.
+  1- The blog post class should have properties like title, content, author, and date.
+  2- It should also have methods to add, edit, and delete blog posts.
+  -------------------------------------------------------------------------------------
+*/
+
