0adb11e1adade4b7010169a4aa18d7d25ba3646c
hady-asaker <hadyasaker8@gmail.com>
2023-04-26 09:01:10 +0200

    ADD nice problem library

---
+++ 7-library.php
@@ -0,0 +1,8 @@
+<?php
+/* -----------------------------------------------------------------------------------
+  Problem 7: Create a class that represents a library.
+  1- The library class should have properties like books, authors, and borrowers.
+  2- It should also have methods to add and remove books, authors, and borrowers,
+  and to lend and return books.
+  ------------------------------------------------------------------------------------
+*/
