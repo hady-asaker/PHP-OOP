31a1322735dee3c38a666ff028bd8841d8ec8e3f
hady-asaker <hadyasaker8@gmail.com>
2023-04-26 05:34:29 +0200

    ADD user problem

---
+++ 6-user.php
@@ -0,0 +1,17 @@
+<?php
+/* -------------------------------------------------------------------------
+  Problem 6: Create a class that represents a user.
+  1- The user class should have properties like username, email, and password.
+  2- It should also have methods to register, login, and logout.
+  ---------------------------------------------------------------------------
+*/
+
+
+
+
+
+
+
+
+
+
