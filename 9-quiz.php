a4183bb62c48e722a5fe182af6b5a5056d7c73e7
hady-asaker <hadyasaker8@gmail.com>
2023-04-27 09:21:43 +0200

    ADD new Problem

---
+++ 9-quiz.php
@@ -0,0 +1,9 @@
+<?php
+/* ---------------------------------------------------------------------------------
+  Problem 9: Create a class that represents a quiz.
+  1- The quiz class should have properties like questions, answers, and scores.
+  2- It should also have methods to add and remove questions, check the answers,
+    and calculate the final score.
+  ----------------------------------------------------------------------------------
+*/
+
