3f90c7f70be627cd58a1ce77d9e18c525069f229
hady-asaker <hadyasaker8@gmail.com>
2023-04-27 05:53:25 +0200

    New Problem

---
+++ 8-calendar.php
@@ -0,0 +1,10 @@
+<?php
+/* --------------------------------------------------------------------------------------------------------
+  Solved By Hady Asaker
+  --------------------------------------------------------------------------------------------------------
+  Problem 8: Create a class that represents a calendar.
+  1- The calendar class should have properties like date, month, and year.
+  2- It should also have methods to display the current date, move forward or backward in time,
+   and check if a given date is a holiday.
+  --------------------------------------------------------------------------------------------------------
+*/
