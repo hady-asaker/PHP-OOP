d9e8890902aa09599c1a49709e365cba7724ed6e
hady-asaker <hadyasaker8@gmail.com>
2023-04-27 09:22:31 +0200

    ADD hotel Pronlem

---
+++ 10-hotel.php
@@ -0,0 +1,8 @@
+<?php
+/* --------------------------------------------------------------------------------------
+  Problem 10: Create a class that represents a hotel.
+  1- The hotel class should have properties like rooms, guests, and reservations.
+  2- It should also have methods to book and cancel rooms, check in and check out guests,
+   and display the current occupancy rate.
+  ---------------------------------------------------------------------------------------
+*/
