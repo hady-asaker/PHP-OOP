65176c30c2c357edadd67e0cd1abae2ae0df26cf
hady-asaker <hadyasaker8@gmail.com>
2023-04-27 09:28:38 +0200

    ADD hotel Problem

--- 10-hotel.php
+++ 10-hotel.php
@@ -3,6 +3,6 @@
   Problem 10: Create a class that represents a hotel.
   1- The hotel class should have properties like rooms, guests, and reservations.
   2- It should also have methods to book and cancel rooms, check in and check out guests,
-   and display the current occupancy rate.
+   and display the current occupancy rate. 
   ---------------------------------------------------------------------------------------
 */
