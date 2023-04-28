e65f83a03790a6cd6896c0120833113a1069e47e
hady-asaker <hadyasaker8@gmail.com>
2023-04-28 13:11:56 +0200

    Solved

--- 10-hotel.php
+++ 10-hotel.php
@@ -11,34 +11,73 @@
 
 class Hotel{
 
-  private $allRooms = 500;
-  public $availableRooms;
+  private $allRooms = 50;
+  public $availableRooms = 2;
   public $Guests = [];
   public $reservations = [];
 
-  public function book()
+  public function booking($name)
   {
-    # code...
+    if ($this->availableRooms > 0 ) {
+      if(!in_array($name, $this->reservations)){
+        $this->reservations[] = $name;
+        --$this->availableRooms;
+      }
+      else{
+        echo "There is already a reservation with this name[$name]" . "<br>";
+      }
+    }
+    else {
+      echo "No Available Room" . "<br>";
+    }
+    
   }
 
-  public function cancelBooking()
+  public function cancelBooking($name)
   {
-    # code...
+    $searching = array_search($name, $this->reservations);    // returning index
+    if($searching){
+      echo $searching;  // 2
+      array_splice($this->reservations, $searching, 1);
+      ++$this->availableRooms;
+    }
+    else{
+      echo "There is no reservation with this name[$name]" . "<br>";
+    }
   }
 
-  public function checkIn()
+  public function checkIn($name)
   {
-    # code...
+    if ($this->availableRooms > 0 || in_array($name, $this->reservations)) {
+      if (!in_array($name, $this->Guests)) {
+        if (in_array($name, $this->reservations)) {
+          array_splice($this->reservations, array_search($name, $this->reservations), 1);
+          ++$this->availableRooms;
+        }
+        $this->Guests[] = $name;
+        --$this->availableRooms;
+      } else {
+        echo "You Are already Checked In";
+      }
+    } else {
+      echo "No Available Room" . "<br>";
+    }
   }
 
-  public function checkOut()
+  public function checkOut($name)
   {
-    # code...
+    if (in_array($name, $this->Guests)) {
+      array_splice($this->Guests, array_search($name, $this->Guests), 1);
+      ++$this->availableRooms;  
+    } else {
+      echo "You didn't Checked in to check out";
+    }
+
   }
 
   public function currentOccupancyRate()
   {
-    # code...
+    return $this->availableRooms;
   }
 
 }
@@ -46,7 +85,27 @@ class Hotel{
 
 $test = new Hotel;
 
+// $test->booking("Hady");
+// $test->cancelBooking("Hady");
+// $test->booking("Hady");
+// $test->cancelBooking("Hady");
 
+// $test->booking("ahmed");
+// $test->booking("kar");
+$test->checkIn("kar");
+$test->checkIn("ahmed");
+// $test->checkIn("koko");
+$test->checkOut("ahmed");
+$test->checkOut("kar");
+$test->checkOut("kara");
+
+// $test->booking("far");
+// $test->booking("bar");
+// $test->cancelBooking("far");
+// // $test->booking("ffffffffff");
+// $test->cancelBooking("kar");
+// $test->cancelBooking("ahmed");
+// $test->cancelBooking("far");
 
 echo "<pre>"; print_r($test); echo "</pre>";
 
