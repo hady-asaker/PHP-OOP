b9a737540cbb4ae9fda8c329b5625ae4d03aeca2
hady-asaker <hadyasaker8@gmail.com>
2023-04-29 04:40:47 +0200

    New problem

---
+++ 15-Validation.php
@@ -0,0 +1,7 @@
+<?php
+/* -------------------------------------------------------------------------------
+ Problem 15: Create a trait called Validation that has methods like validateEmail()
+  and validatePhoneNumber(). 
+  Implement the Validation trait in several classes, such as User, Order, and Payment.
+  --------------------------------------------------------------------------------
+*/
