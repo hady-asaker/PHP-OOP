7a96452bd14858733b51922f15f0d9c4c1aaa24d
hady-asaker <hadyasaker8@gmail.com>
2023-04-29 04:27:00 +0200

    New problem

---
+++ 13-DBConnection.php
@@ -0,0 +1,8 @@
+<?php
+/* -------------------------------------------------------------------------------
+ Problem 13: Create an interface called DatabaseConnection that has methods like
+  connect() and disconnect(). 
+  Implement the DatabaseConnection interface in several classes,
+  such as MySQLConnection and PostgreSQLConnection.
+  --------------------------------------------------------------------------------
+*/
