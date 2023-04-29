19e38143bb04a16ebf7f8ce78b53047d8e119a5f
hady-asaker <hadyasaker8@gmail.com>
2023-04-29 04:10:02 +0200

    Shape Problem To Get Area and Perimeter

---
+++ 11-Shape.php
@@ -0,0 +1,8 @@
+<?php
+/* --------------------------------------------------------------------------------------
+  Problem 11: Create an interface called Shape that has methods like getArea()
+  and getPerimeter().
+  Implement the Shape interface in several classes,
+  such as Rectangle, Triangle, and Circle.
+  ---------------------------------------------------------------------------------------
+*/
