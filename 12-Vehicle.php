bdc113dcfd35bf39405ff374fff579c512186aaf
hady-asaker <hadyasaker8@gmail.com>
2023-04-29 04:24:41 +0200

    New problem

---
+++ 12-Vehicle.php
@@ -0,0 +1,9 @@
+<?php
+/* ---------------------------------------------------------------------------------
+ Problem 12: Create an abstract class called Vehicle that has properties like make,
+  model, and year. Implement methods like startEngine() and stopEngine() that will
+  be overridden by child classes.
+  Create several child classes that inherit from the Vehicle class,
+  such as Car, Truck, and Motorcycle.
+  ----------------------------------------------------------------------------------
+*/
