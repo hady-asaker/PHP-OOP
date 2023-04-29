4efc675218ba3b6dd6e8f0413ddc0c3eb9f47695
hady-asaker <hadyasaker8@gmail.com>
2023-04-29 04:46:24 +0200

    New Problem

---
+++ 16-Employee.php
@@ -0,0 +1,9 @@
+<?php
+/* --------------------------------------------------------------------------------------
+ Problem 16: Create a class called Employee that has properties like name, age, and salary.
+  Implement a calculateBonus() method that will be overridden by child classes.
+  Create several child classes that inherit from the Employee class, such as Manager,
+  Engineer, and Salesperson. 
+  Implement the concept of encapsulation to hide salary data from unauthorized access.
+  ---------------------------------------------------------------------------------------
+*/
