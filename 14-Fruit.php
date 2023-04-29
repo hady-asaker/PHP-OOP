6accc3e9b99b117227a4b32fd4dd0a5a7e23f2a8
hady-asaker <hadyasaker8@gmail.com>
2023-04-29 04:32:58 +0200

    New problem

---
+++ 14-Fruit.php
@@ -0,0 +1,9 @@
+<?php
+/* -------------------------------------------------------------------------------
+ Problem 14: Create an abstract class called Fruit that has properties like name,
+  color, and taste. 
+  - Implement a getNutrition() method that will be overridden by child classes. 
+  - Create several child classes that inherit from the Fruit class, such as Apple,
+    Banana, and Orange.
+  --------------------------------------------------------------------------------
+*/
